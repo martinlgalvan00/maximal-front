f7130a4119709e7c9b2827e6c61757a3c46e74b5
martinlgalvan00 <martinlgalvan00@gmail.com>
2023-01-11 14:30:48 -0300

    vit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  base: "/",
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets"
+  },
   plugins: [react()],
   build:{
     chunkSizeWarningLimit: 1600,
