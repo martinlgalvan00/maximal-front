eb91beea91ab66b760ace8a2bc27d4e6f01d3881
martinlgalvan00 <martinlgalvan00@gmail.com>
2023-01-12 17:15:09 -0300

    a

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@
 function Logo(){
     
     return (
-        <div className='d-flex flex-column align-items-center'>
+        <div className='d-flex flex-column align-items-center bg-gray'>
             <h1 className="col-12 text-center mt-5">Maximal STR Corp</h1>
             <div className="col-10 col-lg-5">
                 <img className="img-fluid" src="https://images2.imgbox.com/b7/19/WJnZ6YI7_o.png" alt="Maximal STR Corp"/>
