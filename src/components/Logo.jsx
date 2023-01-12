7378afa93bfc44881d28e1e014e89d07dc60f537
martinlgalvan00 <martinlgalvan00@gmail.com>
2023-01-12 17:09:44 -0300

    j

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo(){
     return (
         <div className='d-flex flex-column align-items-center'>
             <h1 className="text-center mt-5">Maximal STR Corp</h1>
-            <a href="https://imgbox.com/Zg1PO2WV" target="_blank"><img className="img-fluid" src="https://thumbs2.imgbox.com/c4/57/Zg1PO2WV_t.jpg" alt="image host"/></a>
+            <img className="img-fluid" src="https://thumbs2.imgbox.com/c4/57/Zg1PO2WV_t.jpg" alt="image host"/>
         </div>
     )
 }
