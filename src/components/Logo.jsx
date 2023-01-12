df8a66d41972a28f58c595a12041ef40c7895a62
martinlgalvan00 <martinlgalvan00@gmail.com>
2023-01-12 16:57:14 -0300

    a

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo(){
     return (
         <div className='d-flex flex-column align-items-center'>
             <h1 className="text-center mt-5">Maximal STR Corp</h1>
-            <img className="img-fluid Largo text-center" src="https://drive.google.com/file/d/1SgNgt2Lx0_uRcn_HC9H3NTP4UbyVS8Mv/view?usp=share_link" alt="Maximal STR Corp" />
+            <img className="img-fluid Largo text-center" src="https://share.internxt.com/d/sh/file/3e3aedf15f232644c2b9/b91c03de02d2d097d8407182cc617833f6f91d2aa8d1c9b4939f41dbd8ddef93" alt="Maximal STR Corp" />
         </div>
     )
 }
